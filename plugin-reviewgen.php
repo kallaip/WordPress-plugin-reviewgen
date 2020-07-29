4620eb90a42798e8bb9b44073898adf782a6637b
kajla <kallai.peter@mail.com>
2020-07-29 09:09:47 +0200

    Implemented basic empty admin page

--- plugin-reviewgen.php
+++ plugin-reviewgen.php
@@ -1,6 +1,32 @@
 <?php
 /**
+ * @wordpress-plugin
  * Plugin Name: Review Generator
+ * Description:       A plugint to automatically generate review-style posts from a csv spreadsheet.
+ * Version:           1.0.0
+ * Author:            Peter Kallai
  */
+
+ // If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+   die;
+}
+
+// Include the dependencies needed to instantiate the plugin.
+foreach ( glob( plugin_dir_path( __FILE__ ) . 'admin/*.php' ) as $file ) {
+   include_once $file;
+}
+
+add_action( 'plugins_loaded', 'reviewgen_admin_settings' );
+
+/**
+* Starts the plugin.
+*
+* @since 1.0.0
+*/
+
+function reviewgen_admin_settings() {
+   $plugin = new reviewGen_Submenu( new reviewGen_Submenu_Page() );
+   $plugin->init();
+}
  ?>
- 
